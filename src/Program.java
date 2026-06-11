1f6fccca87db768acb346a71f4830ed81abdffbf
Ovhris <devid8225@gmail.com>
2026-06-11 20:55:19 +0300

    Создал новый класс програм. код перенес в него. хеллоу ворлд удалил

---
+++ src/Program.java
@@ -0,0 +1,9 @@
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Привет, GitHub!");
+        System.out.println("Я учусь Java на javarush сегодня июнь.");
+
+    }
+
+
+}

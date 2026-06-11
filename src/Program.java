ba7ded1aec9857f33aa30627c4ae754e8b4b1a8f
Ovhris <devid8225@gmail.com>
2026-06-11 23:06:53 +0300

    Создал новый класс програм. код перенес в него. хеллоу ворлд удалил добавил ветки и вывод строки

--- src/Program.java
+++ src/Program.java
@@ -2,6 +2,7 @@ public class Program {
     public static void main(String[] args) {
         System.out.println("Привет, GitHub!");
         System.out.println("Я учусь Java на javarush сегодня июнь.");
+        System.out.println("Эксепримент по коммитам продолжается");
 
     }
 

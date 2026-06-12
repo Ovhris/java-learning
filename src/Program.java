d543fb19ea917f64bcdf448731972556876232d2
Ovhris <devid8225@gmail.com>
2026-06-12 22:24:06 +0300

    добавил изменение 12 июня

--- src/Program.java
+++ src/Program.java
@@ -4,6 +4,7 @@ public class Program {
         System.out.println("Я учусь Java на javarush сегодня июнь.");
         System.out.println("Эксперимент по коммитам продолжается");
         System.out.println("12 июня тренинг по коммитам еще одно изменение 22:17");
+        System.out.println("эксперимент по созданию ветки и слияние с мейн");
 
     }
 
